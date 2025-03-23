a4152deb1be139cf445a57c60703e6f68906aee8
mmercad4 <marco@stonepawtraining.com>
2025-03-23 13:49:53 -0400

    added the ability to register users

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
+const userRoutes = require('./routes/userRoutes')
 require('dotenv').config();
 
 // app
@@ -17,10 +18,12 @@ mongoose
 // middleware
 app.use(morgan('dev'));
 app.use(cors({ origin: true, credentials: true }));
+app.use(express.json())
 
 // routes
 const testRoutes = require('./routes/test');
 app.use('/', testRoutes);
+app.use('/api/users', userRoutes);
 
 // port
 const port = process.env.PORT || 8080;
